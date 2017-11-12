7139f8c55b2bb3b1aab79a961bd789adcc941e6c
Guillaume Thibault <thibault.guillaume.01@gmail.com>
2017-11-12 16:37:37 -0500

    Commentaires

--- src/capra_gas_sensor/scripts/CO2_broadcast.py
+++ src/capra_gas_sensor/scripts/CO2_broadcast.py
@@ -7,20 +7,24 @@ from nav_msgs.msg import Odometry
 
 #TODO : fonction qui ira chercher le CO2 level
 
+# Reçoit les données du capteur CO2 et les publie
+# Inclue dans le message publié la position et l'heure.
 class CO2_sensor:
 
 	def __init__(self):
 		self.CO2_level = 400
 		self.pose = None
 
+	# Callback listening for odom/Odometry
 	def read_pos(self, msg):
 		self.pose = msg.pose
 
+	# Main Loop
 	def run(self):
 		rospy.init_node('CO2_broadcast', anonymous=True)
 		rospy.Subscriber('odom', Odometry, self.read_pos)
 		pub = rospy.Publisher('CO2_level', SensorValue, queue_size=10)
-		rate = rospy.Rate(1) #1Hz a changer selon lebesoin
+		rate = rospy.Rate(1) # 1Hz a changer selon lebesoin
 		while not rospy.is_shutdown():
 			msg = SensorValue()
 			msg.value = self.CO2_level 
