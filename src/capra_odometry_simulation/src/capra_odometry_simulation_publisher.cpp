b1242bdb6729fd33052450af280cdf52cc4ad1bf
Quoc-Minh Ton-That <tonthat.quocminh@gmail.com>
2018-07-19 18:07:12 -0400

    Add tf broadcasting in capra_odometry_simulation_publisher node

--- src/capra_odometry_simulation/src/capra_odometry_simulation_publisher.cpp
+++ src/capra_odometry_simulation/src/capra_odometry_simulation_publisher.cpp
@@ -1,14 +1,17 @@
 // ROS framework tools
-#include "ros/ros.h"
-#include "ros/service_client.h"
+#include <ros/ros.h>
+#include <ros/service_client.h>
 
 // Message to publish
-#include "nav_msgs/Odometry.h"
+#include <nav_msgs/Odometry.h>
+
+// tf broadcasting facilities
+#include <tf/transform_broadcaster.h>
 
 // Message to convert and forward
-#include "gazebo_msgs/GetLinkState.h"
-#include "gazebo_msgs/GetLinkStateRequest.h"
-#include "gazebo_msgs/GetLinkStateResponse.h"
+#include <gazebo_msgs/GetLinkState.h>
+#include <gazebo_msgs/GetLinkStateRequest.h>
+#include <gazebo_msgs/GetLinkStateResponse.h>
 
 #define LINK_NAME (1)
 #define REFERENCE_FRAME (2)
@@ -71,19 +74,68 @@ int main(int argc, char** argv)
             proxy.waitForExistence();
         }
 
+        // Pre-initialize variables to avoid unnecessary
+        // memory allocation and deallocation
+        tf::TransformBroadcaster tf_broadcaster;
+
+        // a tf::Transform needs tf::Vector3 as position
+        // and tf::Quaternion as orientation. tf::Vector3
+        // and tf::Quaternion are not convertible to
+        // ros message types, so we need to do the converting
+        // ourselves
+        tf::Transform transform;
+        tf::Quaternion q;
+
+        // While the calls to the service are successful
         while (proxy.call(link_state_request, link_state_response))
         {
             // Build odometry message with the link state response
             capra_odometry.header.stamp = ros::Time::now();
 
+            // The link_state's pose member is directly convertible
+            // to geometry_msgs::Pose type
             capra_odometry.pose.pose =
                 link_state_response.link_state.pose;
 
             capra_odometry.twist.twist = 
                 link_state_response.link_state.twist;
 
+            // Set the transform's current position with
+            // the position set in the newest link_state's pose
+            transform.setOrigin(
+                tf::Vector3(
+                    link_state_response.link_state.pose.position.x,
+                    link_state_response.link_state.pose.position.y,
+                    link_state_response.link_state.pose.position.z
+                )
+            );
+
+            // Set quaternion's members to newest link_state's
+            // orientation values
+            q.setW(link_state_response.link_state.pose.orientation.w);
+            q.setRPY(
+                link_state_response.link_state.pose.orientation.x,
+                link_state_response.link_state.pose.orientation.y,
+                link_state_response.link_state.pose.orientation.z
+            );
+
+            // Set the transform's current orientation with
+            // the orientation set in the newest Quaternion
+            transform.setRotation(q);
+
+            // Publish on ROS's "/odom" topic
             capra_odometry_publisher.publish(capra_odometry);
 
+            // Broadcast newest transformation to tf
+            tf_broadcaster.sendTransform(
+                tf::StampedTransform(
+                    transform,
+                    ros::Time::now(),
+                    "/odom",
+                    "base_link"
+                )
+            );
+            
             loop_rate.sleep();
         }
     }
