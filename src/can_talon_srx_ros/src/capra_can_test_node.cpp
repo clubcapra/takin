6f5f4a94f226a151b5ad8ae0f1595caa50d0378c
ludosataki <ludovicvanasse@gmail.com>
2018-08-18 13:25:28 -0400

    Testing drive

--- src/can_talon_srx_ros/src/capra_can_test_node.cpp
+++ src/can_talon_srx_ros/src/capra_can_test_node.cpp
@@ -18,17 +18,19 @@ int main(int argc, char **argv)
   can_talon_srx::CanSocketInterface::Init("can0");
   ROS_INFO("CAN interface setup succesful!");
 
-  CanTalonSRX testTalon1(11);
-  CanTalonSRX testTalon2(12);
-  CanTalonSRX testTalon3(61);
+  // CanTalonSRX testTalon1(11);
+  // CanTalonSRX testTalon2(12);
+  // CanTalonSRX testTalon3(61);
   CanTalonSRX testTalon4(62);
 
   std::atomic<bool> running(true);
   auto thr = std::thread([&]() {
-    testTalon1.SetModeSelect(CanTalonSRX::kMode_DutyCycle, 100);
-    testTalon2.SetModeSelect(CanTalonSRX::kMode_DutyCycle, 100);
-    testTalon3.SetModeSelect(CanTalonSRX::kMode_DutyCycle, 100);
-    testTalon4.SetModeSelect(CanTalonSRX::kMode_VelocityCloseLoop, 100);
+    // testTalon1.SetModeSelect(CanTalonSRX::kMode_DutyCycle, 100);
+    // testTalon2.SetModeSelect(CanTalonSRX::kMode_DutyCycle, 100);
+    // testTalon3.SetModeSelect(CanTalonSRX::kMode_DutyCycle, 100);
+    testTalon4.SetModeSelect(CanTalonSRX::kMode_DutyCycle, 100);
+    testTalon4.GetEncPosition(0);
+    
     int count = 0;
     while (running)
     {
@@ -37,29 +39,17 @@ int main(int argc, char **argv)
 
       if ((count % 10) == 0)
       {
-        int vals[4];
-        testTalon1.GetEncPosition(vals[0]);
-        testTalon2.GetEncPosition(vals[1]);
-        testTalon3.GetEncPosition(vals[2]);
-        testTalon4.GetEncPosition(vals[3]);
-        ROS_INFO("position: %d %d %d %d %d %d %d %d",
-            vals[0], vals[1], vals[2], vals[3]);
+        int val;
+        testTalon4.GetEncPosition(val);
+        ROS_INFO("position: %d",val);
       }
-      if ((count % 200) == 100)
+      if ((count % 10) == 0)
       {
-        testTalon1.SetDemand(200);
-        testTalon2.SetDemand(200);
-        testTalon3.SetDemand(200);
-        testTalon4.SetDemand(200);
+        testTalon4.SetDemand(500);
+        ROS_INFO("TRYING TO MOVE THE MOTOR");
       }
-      else if ((count % 200) == 199)
-      {
-        testTalon1.SetDemand(-200);
-        testTalon2.SetDemand(-200);
-        testTalon3.SetDemand(-200);
-        testTalon4.SetDemand(-200);
-      }
-      std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+
+      std::this_thread::sleep_for(std::chrono::milliseconds(50));
       ++count;
     }
   });
