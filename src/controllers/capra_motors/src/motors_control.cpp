ea124d5a2141081dd9bb9e09f295d77ff58a69a4
ludosataki <ludovicvanasse@gmail.com>
2018-11-21 20:58:35 -0500

    Added the master slave configuration

--- src/controllers/capra_motors/src/motors_control.cpp
+++ src/controllers/capra_motors/src/motors_control.cpp
@@ -2,6 +2,7 @@
 #include "ctre/phoenix/unmanaged/Unmanaged.h"
 #include "Platform-linux-socket-can.h"
 #include "ros/ros.h"
+#include "ctre/phoenix/MotorControl/CAN/TalonSRX.h"
 #include "geometry_msgs/Twist.h"
 #include "std_msgs/String.h"
 #include <dynamic_reconfigure/server.h>
@@ -19,13 +20,8 @@
 #define FR 12
 #define FL 11
 
-using namespace ctre::phoenix;
-using namespace ctre::phoenix::platform;
-using namespace ctre::phoenix::motorcontrol;
-using namespace ctre::phoenix::motorcontrol::can;
-
-std::vector<std::unique_ptr<TalonSRX>> left_track;
-std::vector<std::unique_ptr<TalonSRX>> right_track;
+std::vector<std::shared_ptr<TalonSRX>> left_track;
+std::vector<std::shared_ptr<TalonSRX>> right_track;
 
 double clamp(double n, double lower, double upper) {
     return std::max(lower, std::min(n, upper));
@@ -35,8 +31,12 @@ void configCallback(capra_motors::MotorConfig &config, uint32_t level) {
 
     ctre::phoenix::motorcontrol::NeutralMode brakeMode;
     switch (config.brake_mode) {
-        case 1: brakeMode = ctre::phoenix::motorcontrol::NeutralMode::Coast; break;
-        default: brakeMode = ctre::phoenix::motorcontrol::NeutralMode::Brake; break;
+        case 1:
+            brakeMode = ctre::phoenix::motorcontrol::NeutralMode::Coast;
+            break;
+        default:
+            brakeMode = ctre::phoenix::motorcontrol::NeutralMode::Brake;
+            break;
     }
 
     for (auto &motor:left_track) {
@@ -56,8 +56,7 @@ void velocityCallback(const geometry_msgs::Twist &msg) {
     double left_power = angle > 0 ? (1.0 - 2 * angle) * power : power;
     double right_power = angle < 0 ? (1.0 + 2 * angle) * power : power;
 
-    if(linear < 0)
-    {
+    if (linear < 0) {
         left_power = -left_power;
         right_power = -right_power;
     }
@@ -84,13 +83,20 @@ int main(int argc, char **argv) {
     std::string interface = "can0";
     ctre::phoenix::platform::can::SetCANInterface(interface.c_str());
 
-    left_track.push_back(std::make_unique<TalonSRX>(RL));
-    right_track.push_back(std::make_unique<TalonSRX>(RR));
+    left_track.push_back(std::make_shared<TalonSRX>(FL));
+    left_track.push_back(std::make_shared<TalonSRX>(RL));
+    right_track.push_back(std::make_shared<TalonSRX>(FR));
+    right_track.push_back(std::make_shared<TalonSRX>(RR));
+
+    // Assuming will always have an equal number of motors in both tracks
+    for (int i = 1; i < left_track.size(); ++i) {
+        left_track[i]->Follow(*left_track[0].get());
+        right_track[i]->Follow(*right_track[0].get());
+    }
 
     for (auto &motor:left_track) {
         motor->SetInverted(true);
     }
-
     ros::Subscriber remote_control = n.subscribe("cmd_vel", 1000, velocityCallback);
     ros::spin();
     return 0;
