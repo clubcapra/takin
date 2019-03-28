41c20eecd21f09b788ab447610918a9db3c29af8
Ludovic Vanasse <ludovicvanasse@gmail.com>
2019-03-27 22:09:04 -0400

    inital commit for estop node

---
+++ takin_estop/src/estop.cpp
@@ -0,0 +1,27 @@
+#include "ros/ros.h"
+
+#include <string>
+#include <iostream>
+#include <chrono>
+#include <thread>
+#include <vector>
+#include <memory>
+#include <map>
+
+int main(int argc, char **argv) {
+
+/*    ros::init(argc, argv, "takin_estop");
+    ros::NodeHandle nh;
+
+    ros::Rate loop_rate(10);
+
+    while (ros::ok()) {
+
+        ros::spinOnce();
+        loop_rate.sleep();
+    }
+*/
+    return 0;
+}
+
+
