c0938d1a6385233b159f6a477a492f5ff9aa5f1a
Ludovic Vanasse <ludovicvanasse@gmail.com>
2019-03-29 11:47:03 -0400

    Added toggleEstop function

--- takin_estop/src/estop.cpp
+++ takin_estop/src/estop.cpp
@@ -1,26 +1,37 @@
 #include "ros/ros.h"
+#include "std_srvs/Trigger.h"
 
-#include <string>
-#include <iostream>
-#include <chrono>
-#include <thread>
-#include <vector>
-#include <memory>
-#include <map>
+bool estop = true;
+
+bool toggleEstop(std_srvs::Trigger::Request &req,std_srvs::Trigger::Response &res){
+    const bool prev_estop = estop;
+
+    estop = !estop;
+
+    if(estop != prev_estop){
+        res.success = static_cast<unsigned char>(true);
+        res.message = "successfully toggle estop";
+    } else {
+        res.success = static_cast<unsigned char>(false);
+        res.message = "failed toggle estop";
+    }
+    return true;
+}
 
 int main(int argc, char **argv) {
 
-/*    ros::init(argc, argv, "takin_estop");
-    ros::NodeHandle nh;
+    ros::init(argc,argv,"takin_estop");
+    ros::NodeHandle n;
 
-    ros::Rate loop_rate(10);
+    ros::ServiceServer service = n.advertiseService("takin_estop",toggleEstop);
+    ros::spin();
+    /*ros::Rate loop_rate(10);
 
     while (ros::ok()) {
 
         ros::spinOnce();
         loop_rate.sleep();
-    }
-*/
+    }*/
     return 0;
 }
 
