0b948cfe641c5817c4768a47ffb15cef8a97668a
Ludovic Vanasse <ludovicvanasse@gmail.com>
2019-04-16 14:35:24 -0400

    Added function for initialize and toggleEstop

--- takin_estop/src/estop.cpp
+++ takin_estop/src/estop.cpp
@@ -1,44 +1,41 @@
 #include "ros/ros.h"
 #include "std_srvs/Trigger.h"
+#include "jetsonGPIO/jetsonGPIO.c"
 
-bool estop = true;
+bool estop_status = true;
+jetsonTX2GPIONumber estop_pin = jetsonTX2GPIONumber::gpio388;
 
+bool toggleEstop(std_srvs::Trigger::Request &req, std_srvs::Trigger::Response &res) {
+    bool prev_estop = estop_status;
+    estop_status = !estop_status;
 
-
-bool toggleEstop(std_srvs::Trigger::Request &req,std_srvs::Trigger::Response &res){
-    const bool prev_estop = estop;
-
-    estop = !estop;
-
-    if(estop != prev_estop){
+    if (estop_status != prev_estop) {
         res.success = static_cast<unsigned char>(true);
-        res.message = "successfully toggle estop";
+        res.message = "successfully toggle estop_status";
+        (estop_status) ? gpioSetValue(estop_pin,1) : gpioSetValue(estop_pin,0);
     } else {
         res.success = static_cast<unsigned char>(false);
-        res.message = "failed toggle estop";
+        res.message = "failed toggle estop_status";
     }
-
-    // set  GPIO 
 
     return true;
 }
 
+void initializeGPIO() {
+    gpioExport(estop_pin);
+    gpioSetDirection(estop_pin, 1);
+}
+
 int main(int argc, char **argv) {
 
-    ros::init(argc,argv,"takin_estop");
+    ros::init(argc, argv, "takin_estop");
     ros::NodeHandle nh;
 
-    ros::ServiceServer service = nh.advertiseService("takin_estop",toggleEstop);
+    initializeGPIO();
+
+    ros::ServiceServer service = nh.advertiseService("takin_estop", toggleEstop);
     ros::spin();
 
-
-    /*ros::Rate loop_rate(10);
-
-    while (ros::ok()) {
-
-        ros::spinOnce();
-        loop_rate.sleep();
-    }*/
     return 0;
 }
 
