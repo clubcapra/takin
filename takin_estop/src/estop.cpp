ecc455e3255aec4fa10ce022871e14d503a8b85c
LazyEngineerToBe <alexandre.francoeur.3@etsmtl.net>
2019-09-05 11:41:58 -0400

    Fixed typo. Added a hw_version param.

--- takin_estop/src/estop.cpp
+++ takin_estop/src/estop.cpp
@@ -5,27 +5,8 @@
 
 bool estop_value = true; //default value for the Estop.
 const jetsonTX2GPIONumber ESTOP_PIN = jetsonTX2GPIONumber::gpio298; //GPIO pin for the pin 21 on the J1 GPIO header
-
-
-// COMMENTED FOR REFERENCES PURPOSES
-///**
-// * Call back function for to toggle the Estop pin. This will change the electric output on the pin and enable or disable
-// * the estop.
-// */
-//bool toggleEstopEnable(std_srvs::Trigger::Request &req, std_srvs::Trigger::Response &res) {
-//    estop_value = !estop_value;
-//
-//    if (estop_value) {
-//        res.message = "successfully toggle estop to on";
-//        gpioSetValue(ESTOP_PIN, 1);
-//    } else {
-//        res.message = "successfully toggle estop to off";
-//        gpioSetValue(ESTOP_PIN, 0);
-//    }
-//    res.success = static_cast<unsigned char>(true);
-//    return true;
-//}
-
+const jetsonTX2GPIONumber FEEDBACK_PIN = jetsonTX2GPIONumber::gpio388; //GPIO pin for the pin ## on the J1 GPIO header
+int hw_version;
 
 /**
  * Call back function for to toggle the Estop pin. This will change the electric output on the pin and enable or disable
@@ -49,12 +30,14 @@ bool toggleEstopDisable(std_srvs::Trigger::Request &req, std_srvs::Trigger::Resp
     return true;
 }
 
-void publishEstopStatus(ros::Publisher *pub){
+void publishEstopStatus(ros::NodeHandle *nh, ros::Publisher *pub){
     unsigned int status;
-    if (!gpioGetValue(ESTOP_PIN, &status))
+    jetsonTX2GPIONumber pin = nh->getParam("hw_version", hw_version) == 2 ? FEEDBACK_PIN : ESTOP_PIN;
+
+    if (!gpioGetValue(pin, &status))
     {
         std_msgs::Bool msg;
-        msg.data == (status == true);
+        msg.data = (bool) status;
         pub->publish(msg);
     }
 }
@@ -77,7 +60,7 @@ int main(int argc, char **argv) {
     ros::ServiceServer serviceEnable = nh.advertiseService("takin_estop_enable", toggleEstopEnable);
     ros::ServiceServer serviceDisable = nh.advertiseService("takin_estop_disable", toggleEstopDisable);
     
-    publishEstopStatus(&estop_status_pub);
+    publishEstopStatus(&nh, &estop_status_pub);
     ros::spin();
 
     return 0;
