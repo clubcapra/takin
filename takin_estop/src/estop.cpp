24e209f45732b1aee47f2bbd5a6fb7799ca01c48
Ludovic Vanasse <ludovicvanasse@gmail.com>
2019-09-20 17:42:00 -0400

    Reverted the GPIO pin number for the e-stop to the origion number
    - estop.cpp
    	Updated the pin to fit the Jetson setup.

--- takin_estop/src/estop.cpp
+++ takin_estop/src/estop.cpp
@@ -4,8 +4,8 @@
 #include "jetsonGPIO/jetsonGPIO.c"
 
 bool estop_value = true; //default value for the Estop.
-const jetsonTX2GPIONumber ESTOP_PIN = jetsonTX2GPIONumber::gpio388; //GPIO pin for the pin 21 on the J1 GPIO header
-const jetsonTX2GPIONumber FEEDBACK_PIN = jetsonTX2GPIONumber::gpio388; //GPIO pin for the pin ## on the J1 GPIO header
+const jetsonTX2GPIONumber ESTOP_PIN = jetsonTX2GPIONumber::gpio298; //GPIO pin for the pin 21 on the J1 GPIO header
+const jetsonTX2GPIONumber FEEDBACK_PIN = jetsonTX2GPIONumber::gpio298; //GPIO pin for the pin ## on the J1 GPIO header
 int hw_version;
 
 /**
